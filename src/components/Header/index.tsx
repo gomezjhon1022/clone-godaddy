60f8dfc5ab58870c956c98f18f4a4a105841997e
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-02-24 22:58:37 -0500

    Add styles to header-right

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -23,7 +23,17 @@ export function Header() {
       <div className='header-right'>
         <a>Contact Us</a>
         <a>Help</a>
-        <details>Sign In</details>
+        <details>
+          <summary>
+            <span>
+            Sign In
+            </span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="arrow-down" width="2rem" height="2rem">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg>
+
+          </summary>
+        </details>
         <a>chart</a>
       </div>
     </header>
