5ffef5ecd620162ae073ef45b51ee1ce4c8e0bd7
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-02-27 21:45:03 -0500

    Add header-right-nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -21,61 +21,80 @@ export function Header() {
         </nav>
       </div>
       <div className='header-right'>
-        <a>Contact Us</a>
-        <a>Help</a>
-        <details>
-          <summary>
-            <span>
-            Sign In
-            </span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="arrow-down" width="2rem" height="2rem">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+        <div className='header-right-nav'>
+          <a>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="icon-question">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
             </svg>
-          </summary>
-          <div className='details-list-container'>
-            <div className='details-list-figure'></div>
-            <div className='details-list'>
-              <div className='part-1'>
-                <div>
-                  Registered Users
+          </a>
+          <a>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="icon-user">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+          </a>
+          <a>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="image-chart">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>            <span></span>
+          </a>
+        </div>
+        <div className='header-right-list'>
+          <a>Contact Us</a>
+          <a>Help</a>
+          <details>
+            <summary>
+              <span>
+              Sign In
+              </span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="arrow-down" width="2rem" height="2rem">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+              </svg>
+            </summary>
+            <div className='details-list-container'>
+              <div className='details-list-figure'></div>
+              <div className='details-list'>
+                <div className='part-1'>
+                  <div>
+                    Registered Users
+                  </div>
+                  <div>
+                    Have an account? Sign in now.
+                  </div>
+                  <div>
+                    <a>Sign In</a>
+                  </div>
                 </div>
-                <div>
-                  Have an account? Sign in now.
+                <div className='part-2'>
+                  <div>
+                    New Customer
+                  </div>
+                  <div>
+                    New to GoDaddy? Create an account to get started today.
+                  </div>
+                  <div>
+                    <a>Create an Account</a>
+                  </div>
                 </div>
-                <div>
-                  <a>Sign In</a>
+                <div className='part-3'>
+                  <p>INBOX LINKS</p>
+                  <ul>
+                    <li>
+                      <a>Sign in to Office 365 Email</a>
+                    </li>
+                    <li>
+                      <a>Sign in to GoDaddy Webmail</a>
+                    </li>
+                  </ul>
                 </div>
-              </div>
-              <div className='part-2'>
-                <div>
-                  New Customer
-                </div>
-                <div>
-                  New to GoDaddy? Create an account to get started today.
-                </div>
-                <div>
-                  <a>Create an Account</a>
-                </div>
-              </div>
-              <div className='part-3'>
-                <p>INBOX LINKS</p>
-                <ul>
-                  <li>
-                    <a>Sign in to Office 365 Email</a>
-                  </li>
-                  <li>
-                    <a>Sign in to GoDaddy Webmail</a>
-                  </li>
-                </ul>
               </div>
             </div>
-          </div>
-        </details>
-        <a>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="image-chart">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-        </svg>
-        </a>
+          </details>
+          <a>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="image-chart">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
+          </a>
+        </div>
       </div>
     </header>
   )
