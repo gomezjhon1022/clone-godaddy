e49d3489673270be15395e8c4d12b762733ebebf
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-03-04 22:22:29 -0500

    Add component SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header'
+import SearchBar from './components/SearchBar'
 import './App.sass'
 import './styles/main.sass'
 
 function App() {
   return (
     <>
-      <Header>header</Header>
+      <Header></Header>
+      <SearchBar></SearchBar>
     </>
   )
 }
