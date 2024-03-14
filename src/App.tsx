21707f7635f3b3fc186619ebe4c0e94002cab16d
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-03-13 20:37:04 -0500

    Add layout component Content section marquee

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header'
 import { SearchBar } from './components/SearchBar'
 import { Footer } from './components/Footer'
+import { Content } from './components/Content'
 import './App.sass'
 import './styles/main.sass'
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header></Header>
       <SearchBar></SearchBar>
+      <Content></Content>
       <Footer></Footer>
     </>
   )
---
+++ src/components/Content/index.tsx
@@ -0,0 +1,57 @@
+import './Content.sass'
+
+export function Content () {
+  return (
+    <main className="main">
+      <section className='marquee'>
+        <div>
+          <div className='primary-panel'>
+            <div className='prymary-headline'>
+              <h2 className='textHeading'>Websites + Marketing</h2>
+              <h3 className='description'>
+                <span>Create your website.</span>
+              </h3>
+              <p>Let our GoDaddy Guides build your starter website for FREE. Give us a call on 020 7023 9020.</p>
+              <div>
+                <div>
+                  <a>
+                    <span>Start for Free</span>
+                  </a>
+                </div>
+              </div>
+              <p>No charge, no obligation, nothing to cancel.**</p>
+            </div>
+          </div>
+          <div className='secondary-panel'>
+            <div className='secondary-headline'>
+              <h2>Domain Names</h2>
+              <h3>
+                <span>
+                  Grab a .co.uk for &#x202A;£0.01&#x202C;/1st yr.
+                </span>
+              </h3>
+              <div className='description'>
+                <span>
+                  3-year purchase required. Additional year(s) &#x202A;£12.99&#x202C;
+                </span>
+              </div>
+              <div>
+                <div>
+                  <a>
+                    <span>Find Your Domain</span>
+                  </a>
+                </div>
+              </div>
+              <div>
+                <span>
+                  Domains include free privacy protection forever
+                  <sup>+</sup>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
