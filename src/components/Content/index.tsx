4c2889ff2314dbbae1e0b955764fcdc38971fcf9
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-03-21 21:15:28 -0500

    Fix view mobile phone secondary-panel

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -52,6 +52,15 @@ export function Content () {
                 </span>
               </div>
             </div>
+            <div className='link-button'>
+              <div>
+                  <a>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="arrow-right">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    </svg>
+                  </a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
