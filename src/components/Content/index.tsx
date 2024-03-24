13a1c88aeaa2db9da7b5baa83cf44dbbe75c24f7
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-03-24 16:53:03 -0500

    Add layout product-grid section

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -64,6 +64,62 @@ export function Content () {
           </div>
         </div>
       </section>
+      <section className='main-content'>
+        <section className='product-grid-container'>
+          <div className='product-grid'>
+            <a>
+              <span>
+                .co.uk <br/>&#x202A;£0.01&#x202C;/1st yr
+              </span>
+            </a>
+            <a>
+              <span>
+                .com <br/>&#x202A;£0.01&#x202C;/1st yr
+              </span>
+            </a>
+            <a>
+              <span>
+                Professional Email <br/><span className="no-wrap-text">£1.99</span> per user/mo
+              </span>
+            </a>
+            <a>
+              <span>
+                Web Hosting <br/><span className="no-wrap-text">£3.99</span>/mo <br/>Includes free SSL
+              </span>
+            </a>
+            <a>
+              <span>
+                SSL Security <br/><span className="no-wrap-text">£49.99</span>/yr
+              </span>
+            </a>
+            <a>
+              <span>
+                WordPress <br/><span className="no-wrap-text">£3.99</span>/mo
+              </span>
+            </a>
+            <a>
+              <span>
+                Microsoft 365 <br/><span className="no-wrap-text">£6.99</span>/mo
+              </span>
+            </a>
+            <a>
+              <span>
+                Website Design Services
+              </span>
+            </a>
+            <a>
+              <img/>
+              <div>
+                <span>
+                  <p>&#x202A;£52.00&#x202C;/1st yr, 2-yr term.<br/>
+                    .ai is the future.
+                  </p>
+                </span>
+              </div>
+            </a>
+          </div>
+        </section>
+      </section>
     </main>
   )
 }
