78ace33f5341d9aeda4e3417d6529490563bd298
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-04-21 16:10:04 -0500

    Add styles to pause-play section

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -405,7 +405,7 @@ export function Content () {
           </div>
           <div className='pause-play'>
             <button className='play-button'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className='pause'>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
               </svg>
             </button>
