5e3fae332762004a20a8e9c28013d04da0f28860
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-05-19 15:30:39 -0500

    Add layout component comments

---
+++ src/components/Content/Comments/index.tsx
@@ -0,0 +1,154 @@
+import './Comments.sass'
+
+export function Comments () {
+  return (
+    <section className="comments">
+      <div>
+        <div className='comments-headline'>
+          <h2>
+            <span>Trusted by 20+ million customers around the world.</span>
+          </h2>
+        </div>
+        <div className='comments-list'>
+          <div className='comments-slide'>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>Great approach for the small business</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>Great approach for the small business.  Nicely packaged to get email and website up and running quickly. </p>
+              </div>
+
+              <p className='comments-autor'>Kalpesh Mistry</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>We switched to Godaddy from our…</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>We switched to Godaddy from our previous provider and we could not be more pleased. They offer a vast product line to help you meet whatever your goals are. The customer service is by far the best. Im so glad we switched</p>
+              </div>
+
+              <p className='comments-autor'>Brittan Hill</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>Godaddy is proactive and helps you…</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>Godaddy is proactive and helps you figure out what you need to create and maintain an online presence. For a busy professional they make it easy and you don't need to have your own IT department to the job.</p>
+              </div>
+
+              <p className='comments-autor'>Dr E.</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>GoDaddy is just the best-easy website…</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>GoDaddy is just the best-easy website building tools, great 24/7 support for any tech questions, and an all inclusive package that includes SEO support at a great price. </p>
+              </div>
+
+              <p className='comments-autor'>Mary Caldwell</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>I love Go Daddy</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>I love Go Daddy! Great products. Simple and easy to set up domains, websites, etc. Awesome customer service. When you need help it’s seriously just a call away!!!</p>
+              </div>
+
+              <p className='comments-autor'>Terri</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>Great resource for the solopreneur</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>Great products for the solo entrepreneur. Good variety of website templates, easy to find something that works with my brand, CMS easy to use, additional upgrades available when I need to make enhancements for my business, monthly metrics emails are a good reminder to check-in, and I like that there are new innovations being launched that can enhance my site experience.</p>
+              </div>
+
+              <p className='comments-autor'>Jill Cashman</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>GoDaddy is the 'Go To</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>I have been writing websites for many years. The decision to have GoDaddy host my website was the right choice. GoDaddy is easy to use and their support staff is the best of any customer service that I have experienced. I highly recommend GoDaddy!</p>
+              </div>
+
+              <p className='comments-autor'>Russell May</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>GoDaddy is the best</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>GoDaddy is the best. They are so easy to use, they are constantly helping me stay up to date, and providing resources to continue to grow my site and marketing. I have had them for years and couldn't be happier. I'm always checking for new updates, they are awesome! Love the ease and accessibility. Would absolutely recommend #1</p>
+              </div>
+
+              <p className='comments-autor'>Richard Bernier</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>Once I found GoDaddy I never looked…</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>Once I found GoDaddy I never looked back. Had tried a few website building programs online but GoDaddy was the most user friendly by far!  Easy to start, affordable, excellent customer service and its very simple to maintain your site.</p>
+              </div>
+
+              <p className='comments-autor'>Charlys Pepper Products</p>
+            </div>
+            <div className='comments-card'>
+              <div>
+                <p className='comments-title'>Godaddy has made running a business for…</p>
+                <div className='comments-rating'>
+                  starts
+                </div>
+                <p className='comments-description'>Godaddy has made running a business for me super simple. There is a lot that goes into being a small business owner. Godaddy's efficiency helps me to be more present for my clients.</p>
+              </div>
+
+              <p className='comments-autor'>La'Toya Guillory</p>
+            </div>
+          
+          </div>
+
+          <div className='comments-rating-overall'>
+            <div >
+              <div>
+                <a>
+                  <div>image</div>
+                  <div>image</div>
+                </a>
+                <div>
+                  <a>
+                    4.7 out of 5 stars based on
+                    <span>97,781</span>
+                    reviews
+                  </a>
+                  <span>|</span>
+                  Showing our 4 and 5 stars reviews.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+
+
+<div class="sfd1jby"><div data-cy="trustpilot-card" class="c1kh9wir" style="--c1kh9wir-1:var(--ux-l7zq7p, white);--c1kh9wir-2:var(--ux-1glcx6s, black)"><div><p class="captitle">Godaddy has made running a business for…</p><div role="img" aria-label="Rating 5 of out 5 stars." class="r19zknh7"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"><path d="M18.0002 0H9.00537V18.0001H18.0002V0Z" fill="#00B67B"></path><path d="M9.00524 0H0V18.0001H9.00524V0Z" fill="#00B67B"></path><path d="M9.00575 12.1358L11.7397 11.4366L12.8771 14.9636L9.00575 12.1358ZM15.2666 7.57576H10.4771L9.00575 3.03662L7.56575 7.56533H2.70312L6.60575 10.3723L5.12401 14.8697L9.01619 12.0627L11.4162 10.3306L15.298 7.56533L15.2666 7.57576Z" fill="#fff"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"><path d="M18.0002 0H9.00537V18.0001H18.0002V0Z" fill="#00B67B"></path><path d="M9.00524 0H0V18.0001H9.00524V0Z" fill="#00B67B"></path><path d="M9.00575 12.1358L11.7397 11.4366L12.8771 14.9636L9.00575 12.1358ZM15.2666 7.57576H10.4771L9.00575 3.03662L7.56575 7.56533H2.70312L6.60575 10.3723L5.12401 14.8697L9.01619 12.0627L11.4162 10.3306L15.298 7.56533L15.2666 7.57576Z" fill="#fff"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"><path d="M18.0002 0H9.00537V18.0001H18.0002V0Z" fill="#00B67B"></path><path d="M9.00524 0H0V18.0001H9.00524V0Z" fill="#00B67B"></path><path d="M9.00575 12.1358L11.7397 11.4366L12.8771 14.9636L9.00575 12.1358ZM15.2666 7.57576H10.4771L9.00575 3.03662L7.56575 7.56533H2.70312L6.60575 10.3723L5.12401 14.8697L9.01619 12.0627L11.4162 10.3306L15.298 7.56533L15.2666 7.57576Z" fill="#fff"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"><path d="M18.0002 0H9.00537V18.0001H18.0002V0Z" fill="#00B67B"></path><path d="M9.00524 0H0V18.0001H9.00524V0Z" fill="#00B67B"></path><path d="M9.00575 12.1358L11.7397 11.4366L12.8771 14.9636L9.00575 12.1358ZM15.2666 7.57576H10.4771L9.00575 3.03662L7.56575 7.56533H2.70312L6.60575 10.3723L5.12401 14.8697L9.01619 12.0627L11.4162 10.3306L15.298 7.56533L15.2666 7.57576Z" fill="#fff"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"><path d="M18.0002 0H9.00537V18.0001H18.0002V0Z" fill="#00B67B"></path><path d="M9.00524 0H0V18.0001H9.00524V0Z" fill="#00B67B"></path><path d="M9.00575 12.1358L11.7397 11.4366L12.8771 14.9636L9.00575 12.1358ZM15.2666 7.57576H10.4771L9.00575 3.03662L7.56575 7.56533H2.70312L6.60575 10.3723L5.12401 14.8697L9.01619 12.0627L11.4162 10.3306L15.298 7.56533L15.2666 7.57576Z" fill="#fff"></path></svg></div><p class="q3e3hkd" style="--q3e3hkd-0:var(--fixed-space-200);--q3e3hkd-2:1.875rem">Godaddy has made running a business for me super simple. There is a lot that goes into being a small business owner. Godaddy's efficiency helps me to be more present for my clients.</p></div><p class="ux-text ux-text-paragraph ux-text-size-1 a5fnxl0" data-cy="attribution-block" style="--a5fnxl0-0:initial;--a5fnxl0-1:initial;--a5fnxl0-2:initial;--a5fnxl0-3:unset;--a5fnxl0-4:unset;--a5fnxl0-5:block;--a5fnxl0-7:block">La'Toya Guillory</p></div></div>
--- src/components/Content/MainContent/index.tsx
+++ src/components/Content/MainContent/index.tsx
@@ -2,6 +2,9 @@ import { CustomerSection } from '../CustomerSection'
 import { ProductGrid } from '../ProductGrid'
 import { RecoreInteractive } from '../RecoreInteractive'
 import { TemplateWall } from '../TemplateWall'
+import { Comments } from '../Comments'
+
+
 
 export default function MainContent () {
 
@@ -11,6 +14,8 @@ export default function MainContent () {
         <RecoreInteractive />
         <TemplateWall />
         <CustomerSection />
+        <Comments />
+
       </section>
   )
 }
