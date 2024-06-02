ad53d6ec34a4b6ec7b52d123aae0e800a47e031d
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-06-01 21:28:15 -0500

    Add styles to comments-card

--- src/components/Content/Comments/index.tsx
+++ src/components/Content/Comments/index.tsx
@@ -1,4 +1,6 @@
 import './Comments.sass'
+import { StarGreen } from './Stargreen'
+import { Stargray } from  './Stargray'
 
 export function Comments () {
   return (
@@ -11,117 +13,172 @@ export function Comments () {
         </div>
         <div className='comments-list'>
           <div className='comments-slide'>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>Great approach for the small business</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>Great approach for the small business</p>
+                  <div className='comments-rating'>
+                  < StarGreen />
+                  < StarGreen />
+                  < StarGreen />
+                  < StarGreen />
+                  < StarGreen />
+                  </div>
+                  <p className='comments-description comments-m'>Great approach for the small business.  Nicely packaged to get email and website up and running quickly. </p>
                 </div>
-                <p className='comments-description'>Great approach for the small business.  Nicely packaged to get email and website up and running quickly. </p>
-              </div>
 
-              <p className='comments-autor'>Kalpesh Mistry</p>
+                <p className='comments-autor'>Kalpesh Mistry</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>We switched to Godaddy from our…</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>We switched to Godaddy from our…</p>
+                  <div className='comments-rating'>
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < Stargray />
+                  </div>
+                  <p className='comments-description comments-s'>We switched to Godaddy from our previous provider and we could not be more pleased. They offer a vast product line to help you meet whatever your goals are. The customer service is by far the best. Im so glad we switched</p>
                 </div>
-                <p className='comments-description'>We switched to Godaddy from our previous provider and we could not be more pleased. They offer a vast product line to help you meet whatever your goals are. The customer service is by far the best. Im so glad we switched</p>
-              </div>
 
-              <p className='comments-autor'>Brittan Hill</p>
+                <p className='comments-autor'>Brittan Hill</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>Godaddy is proactive and helps you…</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>Godaddy is proactive and helps you…</p>
+                  <div className='comments-rating'>
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                  </div>
+                  <p className='comments-description comments-s'>Godaddy is proactive and helps you figure out what you need to create and maintain an online presence. For a busy professional they make it easy and you don't need to have your own IT department to the job.</p>
                 </div>
-                <p className='comments-description'>Godaddy is proactive and helps you figure out what you need to create and maintain an online presence. For a busy professional they make it easy and you don't need to have your own IT department to the job.</p>
-              </div>
 
-              <p className='comments-autor'>Dr E.</p>
+                <p className='comments-autor'>Dr E.</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>GoDaddy is just the best-easy website…</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>GoDaddy is just the best-easy website…</p>
+                  <div className='comments-rating'>
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                  </div>
+                  <p className='comments-description comments-m'>GoDaddy is just the best-easy website building tools, great 24/7 support for any tech questions, and an all inclusive package that includes SEO support at a great price. </p>
                 </div>
-                <p className='comments-description'>GoDaddy is just the best-easy website building tools, great 24/7 support for any tech questions, and an all inclusive package that includes SEO support at a great price. </p>
-              </div>
 
-              <p className='comments-autor'>Mary Caldwell</p>
+                <p className='comments-autor'>Mary Caldwell</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>I love Go Daddy</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>I love Go Daddy</p>
+                  <div className='comments-rating'>
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                  </div>
+                  <p className='comments-description comments-m'>I love Go Daddy! Great products. Simple and easy to set up domains, websites, etc. Awesome customer service. When you need help it’s seriously just a call away!!!</p>
                 </div>
-                <p className='comments-description'>I love Go Daddy! Great products. Simple and easy to set up domains, websites, etc. Awesome customer service. When you need help it’s seriously just a call away!!!</p>
-              </div>
 
-              <p className='comments-autor'>Terri</p>
+                <p className='comments-autor'>Terri</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>Great resource for the solopreneur</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>Great resource for the solopreneur</p>
+                  <div className='comments-rating'>
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                  </div>
+                  <p className='comments-description comments-s'>Great products for the solo entrepreneur. Good variety of website templates, easy to find something that works with my brand, CMS easy to use, additional upgrades available when I need to make enhancements for my business, monthly metrics emails are a good reminder to check-in, and I like that there are new innovations being launched that can enhance my site experience.</p>
                 </div>
-                <p className='comments-description'>Great products for the solo entrepreneur. Good variety of website templates, easy to find something that works with my brand, CMS easy to use, additional upgrades available when I need to make enhancements for my business, monthly metrics emails are a good reminder to check-in, and I like that there are new innovations being launched that can enhance my site experience.</p>
-              </div>
 
-              <p className='comments-autor'>Jill Cashman</p>
+                <p className='comments-autor'>Jill Cashman</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>GoDaddy is the 'Go To</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>GoDaddy is the 'Go To</p>
+                  <div className='comments-rating'>
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                  </div>
+                  <p className='comments-description comments-s'>I have been writing websites for many years. The decision to have GoDaddy host my website was the right choice. GoDaddy is easy to use and their support staff is the best of any customer service that I have experienced. I highly recommend GoDaddy!</p>
                 </div>
-                <p className='comments-description'>I have been writing websites for many years. The decision to have GoDaddy host my website was the right choice. GoDaddy is easy to use and their support staff is the best of any customer service that I have experienced. I highly recommend GoDaddy!</p>
-              </div>
 
-              <p className='comments-autor'>Russell May</p>
+                <p className='comments-autor'>Russell May</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>GoDaddy is the best</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>GoDaddy is the best</p>
+                  <div className='comments-rating'>
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                  </div>
+                  <p className='comments-description comments-s'>GoDaddy is the best. They are so easy to use, they are constantly helping me stay up to date, and providing resources to continue to grow my site and marketing. I have had them for years and couldn't be happier. I'm always checking for new updates, they are awesome! Love the ease and accessibility. Would absolutely recommend #1</p>
                 </div>
-                <p className='comments-description'>GoDaddy is the best. They are so easy to use, they are constantly helping me stay up to date, and providing resources to continue to grow my site and marketing. I have had them for years and couldn't be happier. I'm always checking for new updates, they are awesome! Love the ease and accessibility. Would absolutely recommend #1</p>
-              </div>
 
-              <p className='comments-autor'>Richard Bernier</p>
+                <p className='comments-autor'>Richard Bernier</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>Once I found GoDaddy I never looked…</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>Once I found GoDaddy I never looked…</p>
+                  <div className='comments-rating'>
+                    starts
+                  </div>
+                  <p className='comments-description comments-s'>Once I found GoDaddy I never looked back. Had tried a few website building programs online but GoDaddy was the most user friendly by far!  Easy to start, affordable, excellent customer service and its very simple to maintain your site.</p>
                 </div>
-                <p className='comments-description'>Once I found GoDaddy I never looked back. Had tried a few website building programs online but GoDaddy was the most user friendly by far!  Easy to start, affordable, excellent customer service and its very simple to maintain your site.</p>
-              </div>
 
-              <p className='comments-autor'>Charlys Pepper Products</p>
+                <p className='comments-autor'>Charlys Pepper Products</p>
+              </div>
             </div>
-            <div className='comments-card'>
-              <div>
-                <p className='comments-title'>Godaddy has made running a business for…</p>
-                <div className='comments-rating'>
-                  starts
+            <div className='container-card'>
+              <div className='comments-card'>
+                <div>
+                  <p className='comments-title'>Godaddy has made running a business for…</p>
+                  <div className='comments-rating'>
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                    < StarGreen />
+                  </div>
+                  <p className='comments-description comments-s'>Godaddy has made running a business for me super simple. There is a lot that goes into being a small business owner. Godaddy's efficiency helps me to be more present for my clients.</p>
                 </div>
-                <p className='comments-description'>Godaddy has made running a business for me super simple. There is a lot that goes into being a small business owner. Godaddy's efficiency helps me to be more present for my clients.</p>
-              </div>
 
-              <p className='comments-autor'>La'Toya Guillory</p>
+                <p className='comments-autor'>La'Toya Guillory</p>
+              </div>
             </div>
-          
           </div>
 
           <div className='comments-rating-overall'>
