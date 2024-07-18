91f455489db4563993e8d0c1b851feef6ad99651
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-07-17 22:12:32 -0500

    Add styles for tablet view

--- src/components/Content/Guide/index.tsx
+++ src/components/Content/Guide/index.tsx
@@ -4,23 +4,35 @@ export function Guide () {
   return (
     <section className="guide">
       <div className='guide-container'>
-        <div className='guide-image'>
-
+        <div className='guide-image-text'>
+          <figcaption>
+            Jamila R.
+            <br/>
+            <span>GoDaddy Guide</span>
+          </figcaption>
         </div>
         <div className='guide-content'>
           <div className='guide-logo'>
             <div className='guide-img-wrapper'>
-              <img />
+              <img  src='https://img1.wsimg.com/cdnassets/m/4c719f5dfa9e9e9e/original/GoDaddy-Guides-Logo.svg'/>
             </div>
           </div>
-          <div className='guide-card'>
-            <h2 className='guide-card-title'>
-              <span>Why go with GoDaddy?</span>
-            </h2>
-            <div className='guide-card-description'>
-              <p>Because we know that even the best technology is only as good as the people behind it. That's why we offer expert supprt, plus a lot more.</p>
+          <div className='guide-card-container'>
+            <div className='guide-card'>
+              <h2 className='guide-card-title'>
+                <span>Why go with GoDaddy?</span>
+              </h2>
+              <div className='guide-card-description'>
+                <p>Because we know that even the best technology is only as good as the people behind it. That's why we offer expert supprt, plus a lot more.</p>
+              </div>
+              <div className='guide-card-button'>
+                <div className='button-get-help'>
+                  <a>
+                    Get Help
+                  </a>
+                </div>
+              </div>
             </div>
-            <button className='guide-card-button'>Get Help</button>
           </div>
         </div>
       </div>
